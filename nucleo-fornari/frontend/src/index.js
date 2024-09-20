9af6c7833f740093e0c391a5b83fb88dbbd9249d
Conde <157609877+fvconde@users.noreply.github.com>
2024-09-20 17:35:37 -0300

    create sideMenu component

--- nucleo-fornari/frontend/src/index.js
+++ nucleo-fornari/frontend/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import ProfessorPage from './views/professor/professorView';
 import './index.css'
-import Formulario from './components/Formulario';
+// import Formulario from './components/Login/Formulario';
 // import ParentsPage from './views/parents/parentsView';
 // import SecretaryPage from './views/secretary/secretaryView'
+import SideMenu from './components/SideMenu/sideMenuView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <SideMenu></SideMenu>
   </React.StrictMode>
 );
