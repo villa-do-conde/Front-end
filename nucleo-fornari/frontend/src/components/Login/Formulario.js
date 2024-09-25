2d7a96f1375abd8b5eef95bdf495f63660f015a3
Carlos Eduardo <carlos.quinteros@sptech.school>
2024-09-25 10:21:00 -0300

    TASK: Requisição para login

--- nucleo-fornari/frontend/src/components/Login/Formulario.js
+++ nucleo-fornari/frontend/src/components/Login/Formulario.js
@@ -1,13 +1,46 @@
+import { login } from "../../services/authService";
 import InputText from "./InputText"
 
 const Formulario = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    api.get('/login', {
+      email: email,
+      senha: password
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.status === 200 && response.data?.token) {
+          sessionStorage.setItem('authToken', response.data.token);
+          sessionStorage.setItem('usuario', response.data);
+
+        //   toast.success('Login realizado com sucesso!'); so deus sabe oq eh isso
+          login(response.data.funcao)
+        } else {
+          throw new Error('Ops! Ocorreu um erro interno.');
+        }
+      })
+      .catch(error => {
+        toast.error(error.message);
+      });
+  };
+
     return (
         <section className="flex justify-center my-20">
-            <form className="bg-blue-pastel flex justify-center items-center flex-col max-w-4/5 py-9 px-16 rounded-2xl text-blue-dark">
+            <form onSubmit={handleSubmit} className="bg-blue-pastel flex justify-center items-center flex-col max-w-4/5 py-9 px-16 rounded-2xl text-blue-dark">
                 <h2 className="text-2xl">Entre com sua conta</h2>
-                <InputText label="Email" placeholder="Digite seu email" />
-                <InputText label="Senha" placeholder="Digite sua senha" />
-                <button className="py-3 px-8 rounded bg-blue-main hover:bg-blue-dark hover:text-white-main">Entrar</button>
+                <InputText onChange={e => {setEmail(e.target.value)}} label="Email" placeholder="Digite seu email" />
+                <InputText onChange={p => {setPassword(p.target.value)}} label="Senha" placeholder="Digite sua senha" />
+                <button type="submit" className="py-3 px-8 rounded bg-blue-main hover:bg-blue-dark hover:text-white-main">
+                    Entrar
+                </button>
             </form>
         </section>
     )
--- nucleo-fornari/frontend/src/index.js
+++ nucleo-fornari/frontend/src/index.js
@@ -5,11 +5,12 @@ import './index.css'
 // import Formulario from './components/Login/Formulario';
 // import ParentsPage from './views/parents/parentsView';
 // import SecretaryPage from './views/secretary/secretaryView'
-import SideMenu from './components/SideMenu/sideMenuView';
+// import SideMenu from './components/SideMenu/sideMenuView';
+import Formulario from './components/Login/Formulario'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SideMenu></SideMenu>
+    <Formulario></Formulario>
   </React.StrictMode>
 );
